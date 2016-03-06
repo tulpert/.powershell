1d21bd20d41ceb10563e0843bc0f63189289055f
Tulpert <tulpert@gmail.com>
2016-03-06 21:14:38 +0100

    Added a poweroff function
    A reboot-computer already exists, but not a shutdown-computer
    Therefore I created one mirroring the linux command.

--- PowerShell_profile.ps1
+++ PowerShell_profile.ps1
@@ -1,3 +1,11 @@
+
+
+# Aliases
+# Powershell "Aliases" is just subtitutes for other commands. You can't set an alias to be command with variables.
+# Therefore, I setup functions until Microsoft come to their senses.
+
+# Run an application with elevated privileges
+# Note: applications, not powershell commands.
 function sudo {
 	Param (
 		[string]$Command = "powershell"
@@ -5,3 +13,6 @@ function sudo {
 
 	Start-Process -Verb RunAs $Command
 }
+
+# shutdown the machine
+function poweroff { shutdown /s /t 0 }
